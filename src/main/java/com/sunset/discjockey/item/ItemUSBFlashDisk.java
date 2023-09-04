e60813c519a6a52daa29cf96fae8a6f8935e1bba
dsx137 <70027572+dsx137@users.noreply.github.com>
2023-09-04 12:00:10 +0800

    not finished shit

--- src/main/java/com/sunset/discjockey/item/ItemUSBFlashDisk.java
+++ src/main/java/com/sunset/discjockey/item/ItemUSBFlashDisk.java
@@ -1,28 +1,28 @@
 package com.sunset.discjockey.item;
 
-import net.minecraft.client.gui.Gui;
+import com.sunset.discjockey.client.gui.GUIUSBFlashDisk;
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.fml.DistExecutor;
 
-public class ItemUSBFlashDisk extends Item
-{
+public class ItemUSBFlashDisk extends Item {
     public ItemUSBFlashDisk() {
         super(ItemUSBFlashDisk.GetProperties());
     }
 
     public static Properties GetProperties() {
-        return new Properties();
+        return new Properties()
+                .stacksTo(1);
     }
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
         if (pLevel.isClientSide) {
-
+            Minecraft.getInstance().setScreen(new GUIUSBFlashDisk(pPlayer, pPlayer.getItemInHand(pUsedHand)));
         }
         return super.use(pLevel, pPlayer, pUsedHand);
     }
