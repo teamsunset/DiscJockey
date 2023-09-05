719b2eadc64606bef45133c6e17a4cbfac848c87
dsx137 <70027572+dsx137@users.noreply.github.com>
2023-09-05 17:39:42 +0800

    raw update

--- src/main/java/com/sunset/discjockey/client/gui/GUIUSBFlashDisk.java
+++ src/main/java/com/sunset/discjockey/client/gui/GUIUSBFlashDisk.java
@@ -1,8 +1,9 @@
 package com.sunset.discjockey.client.gui;
 
+import com.sunset.discjockey.client.gui.layout.AbstractLayout;
+import com.sunset.discjockey.client.gui.layout.HorizontalLayout;
 import com.sunset.discjockey.client.gui.layout.VerticalLayout;
 import net.minecraft.client.GameNarrator;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
@@ -13,15 +14,12 @@ import net.minecraft.nbt.StringTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.sunset.discjockey.DiscJockey.DEBUG_LOGGER;
 
 @OnlyIn(Dist.CLIENT)
 public class GUIUSBFlashDisk extends Screen {
@@ -31,9 +29,11 @@ public class GUIUSBFlashDisk extends Screen {
 
     //widget
 
-    private List<EditBox> editBoxes = new ArrayList<>();
+    private List<AbstractLayout> inputLines;
 
     private Button confirmButton;
+
+    private AbstractLayout layout;
 
 
     public GUIUSBFlashDisk(Player pOwner, ItemStack pItemStack) {
@@ -49,14 +49,15 @@ public class GUIUSBFlashDisk extends Screen {
 
     @Override
     protected void init() {
+        layout = new VerticalLayout(this, null, this.width / 4, this.height / 4, this.width / 2, this.height / 2, 10);
+
         this.readFromNBT();
         confirmButton = Button.builder(Component.literal("Confirm"), (button) -> {
             this.saveToNBT();
             this.getMinecraft().popGuiLayer();
         }).build();
 
-        new VerticalLayout(this, this.width / 4, this.height / 4, this.width / 2, this.height / 2, 10)
-                .addAll(editBoxes)
+        layout.addAll(inputLines)
                 .add(confirmButton);
 
         super.init();
@@ -72,28 +73,36 @@ public class GUIUSBFlashDisk extends Screen {
 
 
     //other
-    public EditBox createEmptyEditBox() {
-        return new EditBox(font, 0, 0, 0, 0, Component.literal("url input"));
+    public AbstractLayout createEmptyLine() {
+        HorizontalLayout inputLine = new HorizontalLayout(this, 10);
+        EditBox editBox = new EditBox(font, 0, 0, 0, 0, Component.literal("url input"));
+        Button removeButton = Button.builder(Component.literal("Remove"), (button) -> {
+            inputLine.destroy();
+        }).build();
+        return inputLine
+                .add(editBox)
+                .add(removeButton);
     }
 
     public void readFromNBT() {
+        inputLines = new ArrayList<>();
         CompoundTag compoundTag = this.itemStack.getTag();
         if (compoundTag != null) {
             ListTag listTag = compoundTag.getList("url", Tag.TAG_STRING);
             listTag.forEach(i -> {
-                EditBox editBox = this.createEmptyEditBox();
-                editBox.setValue(i.getAsString());
-                editBoxes.add(editBox);
+                AbstractLayout inputLine = this.createEmptyLine();
+                ((EditBox) inputLine.getWidgets().get(0)).setValue(i.getAsString());
+                inputLines.add(inputLine);
             });
         }
-        editBoxes.add(this.createEmptyEditBox());
+        inputLines.add(this.createEmptyLine());
     }
 
     public void saveToNBT() {
         ListTag listTag = new ListTag();
 
-        editBoxes.forEach(i -> {
-            listTag.add(StringTag.valueOf(i.getValue()));
+        inputLines.forEach(i -> {
+            listTag.add(StringTag.valueOf(((EditBox) i.getWidgets().get(0)).getValue()));
         });
 
         this.itemStack.addTagElement("url", listTag);
