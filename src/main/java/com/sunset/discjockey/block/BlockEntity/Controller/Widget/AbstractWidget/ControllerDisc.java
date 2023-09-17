5250cb69cb291c37a1f007f20acbd76343704dc9
dsx137 <1419696375@qq.com>
2023-09-17 15:07:34 +0800

    try to fill disc code

--- src/main/java/com/sunset/discjockey/block/BlockEntity/Controller/Widget/AbstractWidget/ControllerDisc.java
+++ src/main/java/com/sunset/discjockey/block/BlockEntity/Controller/Widget/AbstractWidget/ControllerDisc.java
@@ -1,19 +1,66 @@
 package com.sunset.discjockey.block.BlockEntity.Controller.Widget.AbstractWidget;
 
+import com.sunset.discjockey.block.BlockEntity.Controller.Audio.ControllerAudio;
+import com.sunset.discjockey.block.BlockEntity.Controller.Audio.ControllerAudioManager;
 import com.sunset.discjockey.block.BlockEntity.Controller.Widget.Base.ControllerWidget;
 import com.sunset.discjockey.block.BlockEntity.Controller.Widget.Base.ControllerWidgetManager;
+import com.sunset.discjockey.util.SpecialType.SimpleInterpolationValue;
 import com.sunset.discjockey.util.TouchMap.Vec2Type.PlaneRange;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.player.Player;
 
-public abstract class ControllerDisc extends ControllerWidget
-{
-    public ControllerDisc(String id, ControllerWidgetManager.InteractType interactType, PlaneRange planeRange) {
-        super(id, interactType, planeRange);
+public class ControllerDisc extends ControllerWidget {
+
+    public SimpleInterpolationValue speed = new SimpleInterpolationValue(0.5, 0.5);
+
+    public ControllerAudioManager controllerAudioManager;
+
+    public int channelIndex;
+
+    public ControllerDisc(String id, PlaneRange planeRange, ControllerAudioManager controllerAudioManager, int channelIndex) {
+        super(id, ControllerWidgetManager.InteractType.SCROLL, planeRange);
+        this.controllerAudioManager = controllerAudioManager;
+        this.channelIndex = channelIndex;
     }
 
     @Override
-    public abstract CompoundTag getCompoundTag();
+    public void executeOnServer(Player player, double value) {
+        this.markExecute();
+        this.markDirty();
+    }
 
     @Override
-    public abstract void writeCompoundTag(CompoundTag compoundTag);
+    public void executeOnClient() {
+        ControllerAudio audio = this.controllerAudioManager.loadedAudios.get(channelIndex);
+        if (audio != null && audio.speakerSound != null) {
+//            audio.speakerSound.fileAudioStream.outArray = ProcessAudio.changeSpeed(audio.speakerSound.fileAudioStream.array, (float) this.speed.get());
+        }
+    }
+
+    @Override
+    public CompoundTag getCompoundTag() {
+        CompoundTag compoundTag = new CompoundTag();
+        compoundTag.putDouble("speed", this.speed.get());
+        return compoundTag;
+    }
+
+    ;
+
+    @Override
+    public void writeCompoundTag(CompoundTag compoundTag) {
+        this.speed.set(compoundTag.getDouble("speed"));
+    }
+
+    ;
+
+    @Override
+    public void onServerTick() {
+        super.onServerTick();
+        this.speed.onServerTick();
+    }
+
+    @Override
+    public void onClientTick() {
+        super.onClientTick();
+    }
 }
